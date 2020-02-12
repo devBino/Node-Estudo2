6bc29b6e7a5dbdcd802a822bb769ad816fafea21
cilada bino <fernando.bino.machado@gmail.com>
2020-02-12 05:39:02 -0300

    retornando dados de algum banco para a view

--- SiteNoticias/app/routes/home.js
+++ SiteNoticias/app/routes/home.js
@@ -1,7 +1,20 @@
 module.exports = function(app){
     
     app.get('/',(req,res)=>{
-        res.render('home/index')
+
+        let mysql   = require('mysql')
+        let con     = mysql.createConnection({
+            host:'127.0.0.1',
+            user:'fer',
+            password:'fer',
+            database:'pinturas'
+        })
+
+        con.connect()
+        
+        con.query('select * from pt_servico', function(error,result){
+            res.render('home/index',{servicos:result})
+        })
     })
 
     app.get('/home', (req,res)=>{
